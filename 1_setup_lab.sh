29ccc02224cb545a55fc75e2547bb3414e4ed6b8
crrincon <cristian.rincon@mercadolibre.com.co>
2022-02-11 19:06:06 -0500

    :sparkles: it's alive

--- 1_setup_lab.sh
+++ 1_setup_lab.sh
@@ -23,6 +23,8 @@ function install_zsh() {
     if [ -z "$(which zsh)" ]; then
         echo "zsh is not installed. Installing zsh..."
         apt install zsh -y
+        $(pwd)/1_setup_zsh.sh
+        $(pwd)/2_setup_zsh_plugins.sh
     fi
 }
 
@@ -42,22 +44,6 @@ function install_curl() {
         echo "curl is not installed. Installing curl..."
         apt install curl -y
     fi
-}
-
-
-# Function to verify if oh-my-zsh is installed and if not, install it
-function install_oh_my_zsh() {
-    echo "Installing oh-my-zsh..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-}
-
-
-# Function to install oh-my-zsh plugins
-function install_oh_my_zsh_plugins() {
-    echo "Installing oh-my-zsh plugins..."
-    apt install zsh-autosuggestions -y
-    echo "Installing zsh-syntax-highlighting..."
-    apt install zsh-syntax-highlighting -y
 }
 
 
